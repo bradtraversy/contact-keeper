1074d50087a487dff04f2410802342db4b30c445
Brad Traversy <traversymedia@gmail.com>
2019-05-28 09:45:54 -0400

    Contact model

---
+++ models/Contact.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
