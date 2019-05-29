cb90c0c687c4cca593732e7976c8e4cf068377fd
Brad Traversy <traversymedia@gmail.com>
2019-05-29 12:34:18 -0400

    PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -28,7 +29,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, loading } = authContext;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        !isAuthenticated && !loading ? (
+          <Redirect to='/login' />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
