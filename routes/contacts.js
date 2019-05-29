b6121903040eebe88771cc83100df797d06d716c
Brad Traversy <traversymedia@gmail.com>
2019-05-29 14:30:16 -0400

    Spelling mistake

--- routes/contacts.js
+++ routes/contacts.js
@@ -115,7 +115,7 @@ router.delete('/:id', auth, async (req, res) => {
 
     res.json({ msg: 'Contact removed' });
   } catch (err) {
-    console.error(er.message);
+    console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
