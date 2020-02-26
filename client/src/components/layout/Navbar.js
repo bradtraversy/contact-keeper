84ec7742f7f47222352d1f7dfcd69403a899efc8
Will Adams <bushblade@gmail.com>
2020-02-26 17:38:24 +0000

    comment

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,7 @@ const Navbar = ({ title, icon }) => {
   const [authState, authDispatch] = useAuth()
   const { isAuthenticated, user } = authState
 
+  // ignore unused var in destructuring
   // eslint-disable-next-line
   const [_, contactDispatch] = useContacts()
 
