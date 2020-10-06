9d073ffd5122fd6a11cb0822a800a11c0622480c
Mohammad Faisal Khan <khanfaisal313313@gmail.com>
2020-10-06 15:39:41 +0530

    Update About.js

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div>
       <h1>About This App</h1>
       <p className='my-1'>
-        This is a full stack React app for keeping contacts
+        This is a full stack React app for keeping and managing contacts
       </p>
       <p className='bg-dark p'>
         <strong>Version: </strong> 1.0.0
