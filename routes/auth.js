c85c050fafa31b563b2567b6bcac6a867e348962
Will Adams <bushblade@gmail.com>
2021-11-13 15:45:00 +0000

    remove unnecessary arrays

--- routes/auth.js
+++ routes/auth.js
@@ -26,53 +26,51 @@ router.get('/', auth, async (req, res) => {
 // @access    Public
 router.post(
   '/',
-  [
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Password is required').exists(),
-  ],
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password is required').exists(),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({errors: errors.array()});
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
-      let user = await User.findOne({email});
+      let user = await User.findOne({ email });
 
       if (!user) {
-        return res.status(400).json({msg: 'Invalid Credentials'});
+        return res.status(400).json({ msg: 'Invalid Credentials' });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return res.status(400).json({msg: 'Invalid Credentials'});
+        return res.status(400).json({ msg: 'Invalid Credentials' });
       }
 
       const payload = {
         user: {
-          id: user.id,
-        },
+          id: user.id
+        }
       };
 
       jwt.sign(
         payload,
         config.get('jwtSecret'),
         {
-          expiresIn: 360000,
+          expiresIn: 360000
         },
         (err, token) => {
           if (err) throw err;
-          res.json({token});
-        },
+          res.json({ token });
+        }
       );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
     }
-  },
+  }
 );
 
 module.exports = router;
